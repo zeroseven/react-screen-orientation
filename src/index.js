4bfa101beff1f8f6465a5c0af1d10ba67c788e4c
Timo Uhlmann <t.uhlmann@zeroseven.de>
2016-03-24 12:25:48 +0100

    Listening to orientationchange events

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Children, Component, PropTypes } from 'react'
 
 export class Orientation extends Component {
   render () {
-    const { type, children, className } = this.props
+    const { orientation, children, className } = this.props
     return (
-      <div className={`${className} react-orientation react-orientation--${type}`}>
+      <div className={`${className} react-orientation react-orientation--${orientation}`}>
         {children}
       </div>
     )
@@ -14,12 +14,16 @@ export class Orientation extends Component {
 Orientation.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
-  type: PropTypes.oneOf(['portrait', 'landscape'])
+  orientation: PropTypes.oneOf(['portrait', 'landscape']),
+  alwaysRender: PropTypes.bool
 }
 
 Orientation.defaultProps = {
-  className: ''
+  className: '',
+  alwaysRender: true
 }
+
+const noop = () => false
 
 const lock = window.screen.lockOrientation ||
   window.screen.mozLockOrientation ||
@@ -38,13 +42,60 @@ export default class DeviceOrientation extends Component {
   constructor (props) {
     super(props)
     this.lockOrientation(props)
+    this.onOrientationChange = this.onOrientationChange.bind(this)
+
+    this.state = {
+      orienation: null,
+      type: null,
+      angle: null
+    }
+  }
+
+  componentWillMount () {
+    this.onOrientationChange(null)
+  }
+
+  componentDidMount () {
+    console.log('DeviceOrientation', 'componentDidMount')
+    if ('onchange' in window.screen.orientation) {
+      console.log('Using screen.orientation.onchange')
+      window.screen.orientation.addEventListener('change', this.onOrientationChange)
+    } else if ('onorientationchange' in window) {
+      console.log('Using window.onorientationchange')
+      window.addEventListener('orientationchange', this.onOrientationChange)
+    } else {
+      console.warn('No orientationchange events')
+    }
+  }
+
+  componentWillUnmount () {
+    console.log('DeviceOrientation', 'componentWillUnmount')
+    if ('onchange' in window.screen.orientation) {
+      console.log('Removing screen.orientation.onchange')
+      window.screen.orientation.removeEventListener('change', this.onOrientationChange)
+    } else if ('onorientationchange' in window) {
+      console.log('Removing window.onorientationchange')
+      window.removeEventListener('orientationchange', this.onOrientationChange)
+    }
+  }
+
+  onOrientationChange (event) {
+    const onOrientationChange = this.props.onOrientationChange || noop
+    const [orientation, type] = window.screen.orientation.type.split('-')
+    const { angle } = window.screen.orientation
+    this.setState({
+      orientation,
+      type,
+      angle
+    })
+    onOrientationChange(orientation, type, angle)
   }
 
   lockOrientation ({lockOrientation}) {
     if (typeof lockOrientation !== 'string') {
       return
     }
-    let onLockOrientation = this.props.onLockOrientation || (() => false)
+    const onLockOrientation = this.props.onLockOrientation || noop
     return lock(lockOrientation).then(function () {
       onLockOrientation(true)
     }).catch(function () {
@@ -90,7 +141,8 @@ DeviceOrientation.propTypes = {
     PropTypes.oneOf(LOCK_ORIENTATIONS),
     PropTypes.arrayOf(PropTypes.oneOf(LOCK_ORIENTATIONS))
   ]),
-  onLockOrientation: PropTypes.func
+  onLockOrientation: PropTypes.func,
+  onOrientationChange: PropTypes.func
 }
 
 DeviceOrientation.defaultProps = {
